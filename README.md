3deec375910c02ad4e63a5575df17102ef7a5d4d
Natan Lao <natanlao@users.noreply.github.com>
2020-06-28 22:12:21 +0000

    Rewrite reddit_transfer.py
    Bad practice to have wide commits like this, I know.
    * Removed click as a dependency
    * Updated praw to 7.x
    * Performance improvements (pretty sure, but too lazy to test)
    * Updated Python to 3.8.x
    * Linting with pycodestyle
    * Transferring configuration
    * Add .gitignore to make sure praw.ini never gets committed

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
-Rudimentary reddit account transfer script. Transfer friends, subreddits, and
-saved posts. Can handle accounts with two-factor authentication.
+# reddit-transfer
 
-# Usage
+Rudimentary reddit account transfer script. Given a new username and an old
+username:
+
+1. unsubscribes from all subreddits on the new user,
+2. copies all subreddit subscriptions from old user to new user,
+3. copies all saved posts and comments from old user to new user,
+4. copies all friends from old user to new user, and
+5. copies preferences from old user to new user.
+
+Copying preferences works... kind of. It copies most settings over.
+
+The script can handle accounts with two-factor authentication, too. Either way,
+it needs application keys (choose type 'script').
+
+## Usage
+
+Running Python 3.8:
 
     $ pip install -r requirements.txt
     $ python reddit_transfer.py <old_username> <new_username>
 
-# Limitations
+## Limitations
 
-* Does not properly handle accounts where the amount of saved posts, subreddits, or friends is greater than 1,000
-* Logging and progress-tracking is very rudimentary
+* It probably can't properly handle accounts with more than 1,000 saved posts,
+  subreddits, or friends. But I haven't tried.
--- reddit_transfer.py
+++ reddit_transfer.py
@@ -1,21 +1,22 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3.8
 import configparser
+import getpass
 import logging
+import sys
 
-import click
 import praw
 
-log = logging.getLogger(__name__)
-logging.basicConfig(level=logging.WARNING)
-user_agent = "la.natan.reddit-transfer:v0.0.1"
+log = logging.getLogger('reddit-transfer')
+logging.basicConfig(level=logging.INFO)
+user_agent = "la.natan.reddit-transfer:v0.0.2"
 
 
 def get_reddit_object(username: str) -> praw.Reddit:
     """
     Returns a Reddit object authenticated for the given username
     from the stored credentials. If no credentials for the given
-    username are stored, we prompt the user for credentials and
-    store them.
+    username are stored, prompt the user for credentials and store
+    them.
 
     Credentials are stored in `praw.ini`. The password is not
     stored; the user will always be prompted for it.
@@ -25,69 +26,78 @@ def get_reddit_object(username: str) -> praw.Reddit:
     if username not in config:
         config[username] = {
             'username': username,
-            'client_id': click.prompt(f"Client ID [{username}]"),
-            'client_secret': click.prompt(f"Client secret [{username}]",
-                                          hide_input=True) 
+            'client_id': input(f"Client ID [{username}]: "),
+            'client_secret': getpass.getpass(f"Client secret [{username}]: ")
         }
         with open('praw.ini', 'w') as fp:
             config.write(fp)
-    password = click.prompt(f"Password [{username}]", hide_input=True)
-    auth_code = click.prompt(f"Auth code [{username}] (optional)", default="", show_default=False)
-    if auth_code:  # https://www.reddit.com/r/redditdev/comments/74cu2i/-/dnxe1lu/
+    password = getpass.getpass(f"Password [{username}]: ")
+    auth_code = input(f"Auth code [{username}] (optional): ")
+    if auth_code:  # /r/redditdev/comments/74cu2i/-/dnxe1lu/
         password = f'{password}:{auth_code}'
     # We don't specify a site to :class:`praw.Reddit` since praw.ini is loaded
     # at the time that the first instance of :class:`praw.Reddit` is
     # instantiated, and there's no way to ask Praw to reload it.
-    return praw.Reddit(password=password, user_agent=user_agent, **config[username])
+    return praw.Reddit(password=password, user_agent=user_agent,
+                       **config[username])
 
 
-@click.command()
-@click.argument('old_user')
-@click.argument('new_user')
-def sync_data(old_user: str, new_user: str):
+def sync_data(old_user: str, new_user: str) -> None:
     old = get_reddit_object(old_user)
     new = get_reddit_object(new_user)
 
-    # Transfer subs
-    # I don't know a better way to do this
-    current = [sub.display_name for sub in new.user.subreddits(limit=None)]
-    click.echo(f"Unsubscribing from {len(current)} subreddits")
-    if len(current) > 0:
-        new.subreddit(current[0]).unsubscribe(other_subreddits=current)  # Unsub from defaults
-    subs = [sub.display_name for sub in old.user.subreddits(limit=None)]
-    click.echo(f"Subscribing to {len(subs)} subreddits")
-    new.subreddit(subs[0]).subscribe(other_subreddits=subs)
+    # We could use set.difference here but I think it's overkill and less clear
+    old_subs = [sub.display_name for sub in old.user.subreddits(limit=None)]
+    log.info(f"{old_user}: found {len(old_subs)} existing subscriptions")
+    new_subs = [sub.display_name for sub in new.user.subreddits(limit=None)]
+    log.info(f"{new_user}: found {len(new_subs)} existing subscriptions")
+    un_sub = [sub for sub in new_subs if sub not in old_subs]
+    log.warning(f"{new_user}: unsubscribing from {len(un_sub)} subreddits")
+    if len(un_sub) > 0:
+        new.subreddit(un_sub[0]).unsubscribe(other_subreddits=un_sub[1:])
+    to_sub = [sub for sub in old_subs if sub not in new_subs]
+    log.warning(f"{new_user}: subscribing to {len(to_sub)} new subreddits")
+    if len(to_sub) > 0:
+        new.subreddit(to_sub[0]).subscribe(other_subreddits=to_sub[1:])
+    # Since these are bulk operations, we could just unsubscribe from all
+    # then resubscribe as needed but I've found that there's some lag between
+    # subscribing to a subreddit and the Reddit API recognizing that we've
+    # subscribed to a subreddit
 
-    # Transfer saved
-    click.echo("Transferring saved posts and comments.")
-    old_saved = old.user.me().saved(limit=None)
-    new_saved = [item.id for item in new.user.me().saved(limit=None)]
-    with click.progressbar(old_saved,
-                           label="Transferring saved posts and comments...") \
-                           as saved:
-        for item in saved:
-            if item.id in new_saved:
-                log.debug(f"Item {item.id} already saved, skipping...")
-                continue
-            if isinstance(item, praw.models.Comment):
-                log.debug("Saving comment %s", item.id)
-                new.comment(item.id).save()
-            elif isinstance(item, praw.models.Submission):
-                log.debug("Saving submission %s", item.id)
-                new.submission(item.id).save()
-            else:
-                raise
+    # I unfortunately could find no API for saving in bulk
+    old_saved = list(old.user.me().saved(limit=None))
+    log.info(f"{old_user}: found {len(old_saved)} existing saved posts, comments")
+    new_saved = list(item.id for item in new.user.me().saved(limit=None))
+    log.info(f"{new_user}: found {len(new_saved)} existing saved posts, comments")
+    new_saved = [item for item in old_saved if item.id not in new_saved]
+    log.warning(f"{new_user}: saving {len(new_saved)} posts and comments")
+    for i, item in enumerate(new_saved, start=1):
+        # This is a little unwieldy, but changing the Reddit instance this way
+        # saves us a couple RTTs, or it should, anyway. Maybe there's a better
+        # way to do this.
+        item._reddit = new
+        item.save()
+        log.debug(f"{new_user}: saved {item.id=} ({i}/{len(new_saved)})")
 
-    # Transfer friends
-    old_friends = [friend.name for friend in old.user.friends()]
-    new_friends = [friend.name for friend in new.user.friends()]
-    with click.progressbar(old_friends, label="Transferring friends...") as friends:
-        for friend in friends:
-            log.debug("Adding friend %s", friend)
-            if friend in new_friends:
-                log.debug("Friend %s is a duplicate!", friend)
-                continue  # Skip duplicate
-            new.redditor(friend).friend()
+    # There is also no API for making friends in bulk :(
+    # TODO: remove friends first?
+    old_friends = list(old.user.friends())
+    log.info(f"{old_user}: found {len(old_friends)} existing friends")
+    new_friends = list(friend.name for friend in new.user.friends())
+    log.info(f"{new_user}: found {len(new_friends)} existing friends")
+    new_friends = [friend for friend in old_friends if friend.name not in new_friends]
+    log.warning(f"{new_user}: befriending {len(new_friends)} new users")
+    for i, friend in enumerate(new_friends, start=1):
+        friend._reddit = new
+        friend.friend()
+        log.debug(f"{new_user}: added {friend=} ({i}/{len(new_friends)})")
+
+    log.warning(f"{new_user}: copying preferences from {old_user}")
+    new.user.preferences.update(**old.user.preferences())
 
 if __name__ == '__main__':
-    sync_data()
+    if len(sys.argv) != 3:
+        print("./reddit_transfer.py [old-username] [new-username]")
+        exit(1)
+    _, old_user, new_user = sys.argv
+    sync_data(old_user, new_user)
